0a1fc9b16f9ab817ace88fb6c0b3644e760442c6
crisredfi <crisredfi1@gmail.com>
2012-10-26 19:07:39 +0100

    updated catchoom licence headers

--- CRSMobile/CRSMobile/ImageTransformations.m
+++ CRSMobile/CRSMobile/ImageTransformations.m
@@ -1,22 +1,12 @@
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Copyright 2012 Catchoom Advertising Netwroks, S.L.
 //
-// Permission to use, copy, modify, and distribute this software and its documentation for any purpose and without fee is hereby
-// granted, provided that the above copyright notice appear in all copies and that both that the copyright notice and this permission
-// notice and warranty disclaimer appear in supporting documentation, and that the name of the author not be used in advertising or
-// publicity pertaining to distribution of the software without specific, written prior permission.
-//
-// The author disclaim all warranties with regard to this software, including all implied warranties of merchantability and fitness.
-// In no event shall the author be liable for any special, indirect or consequential damages or any damages whatsoever resulting from
-// loss of use, data or profits, whether in an action of contract, negligence or other tortious action, arising out of or in connection
-// with the use or performance of this software.
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//© Catchoom Technologies S.L.
+//Licensed under the MIT license.
+//https://raw.github.com/catchoom/ios-crc/LICENSE
 //
 //  ImageTransformations.m
 //
 //  Created by David Marimon Sanjuan on 10/2/12.
 //  Copyright (c) 2012 Catchoom Advertising Network S.L. All rights reserved.
-//  Licensed for use under the Evaluation License for Catchoom Visual Search Software and Service.
 //
 
 #import "ImageTransformations.h"
