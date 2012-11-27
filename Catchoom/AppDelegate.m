2d9726674479606466ff1569c0ae9c1a092c1609
crisredfi <crisredfi1@gmail.com>
2012-11-27 20:49:39 +0000

    updated app delegate, we don't need to began server anymore

--- Catchoom/AppDelegate.m
+++ Catchoom/AppDelegate.m
@@ -18,7 +18,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
 
-    [[CatchoomService sharedCatchoom] beginServerConnection];
    
        return YES;
 }
